494c0f9011e2ab6c8cdc1dd11bd5ca2f4e93b21e
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-21 14:32:46 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,9 @@ const About = () => {
     <div className="details" id="details">
       <h2 className="typed-text p-3 fs-3 fw-bold">Hi! I am Balamurugan..</h2>
       <p className="additional-text fs-6">
-        I'm currently pursuing a B.E. in Computer Science and Engineering at the College of Engineering, Guindy.
-        I’m passionate about Python and its libraries like Django, Flask, Tkinter, NumPy, and Pandas, and have built several projects applying them.
+        Currently pursuing B.E. in Computer Science and Engineering at the College of Engineering, Guindy.
+        <br></br>
+        I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack developer </span> specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend and <span className="fw-bold" style={{color:"#ffd343"}}> Python </span> backend development.
         <br></br>
         <br></br>
         Outside tech, I enjoy remixing tracks in DAWs — giving familiar tunes my own twist.
