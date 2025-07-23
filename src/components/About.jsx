1495404cc4aae68881719e04d62918e3a24bc92a
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 22:42:34 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
       <p className="additional-text fs-6">
         Currently pursuing B.E. in Computer Science and Engineering at the College of Engineering, Guindy.
         <br></br>
+        <br></br>
         I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack</span> & <span className="fw-bold" style={{color: "#7B61FF"}}> LLM </span> Applications developer specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend, <span className="fw-bold" style={{color:"#3776ab"}}> Pyt</span><span className="fw-bold" style={{color: "#ffd343"}}>hon </span> backend development 
         <br></br>
         and integrating <span className="fw-bold" style={{color: "#7B61FF"}}>LLMs</span> into real-world apps.
