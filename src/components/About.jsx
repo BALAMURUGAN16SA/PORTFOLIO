319f27504f349e90bde1ecc2ab6312cd0e561c98
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 22:35:42 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,8 @@ const About = () => {
       <p className="additional-text fs-6">
         Currently pursuing B.E. in Computer Science and Engineering at the College of Engineering, Guindy.
         <br></br>
-        I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack</span> & <span className="fw-bold" style={{color: "#7B61FF"}}> LLM Applications </span> developer specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend and <span className="fw-bold" style={{color:"#3776ab"}}> Pyt</span><span className="fw-bold" style={{color: "#ffd343"}}>hon </span> backend development.
+        I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack</span> & <span className="fw-bold" style={{color: "#7B61FF"}}> LLM Applications </span> developer specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend and <span className="fw-bold" style={{color:"#3776ab"}}> Pyt</span><span className="fw-bold" style={{color: "#ffd343"}}>hon </span> backend development and and integrating <span className="fw-bold" style={{color: "#7B61FF"}}>LLMs</span> into real-world apps.
         <br></br>
-        I’ve also worked on projects that integrate LLMs into real world applications.
         <br></br>
         Outside tech, I enjoy remixing tracks in DAWs — giving familiar tunes my own twist.
         <br></br>
