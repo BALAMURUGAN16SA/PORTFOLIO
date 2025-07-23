17f0d1ff58cfdc1ea7b5ca32cf171087f9c0ff17
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 22:28:32 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <p className="additional-text fs-6">
         Currently pursuing B.E. in Computer Science and Engineering at the College of Engineering, Guindy.
         <br></br>
-        I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack</span> & <span className="fw-bold" style={{color: "#7B61FF"}}> LLM Applications </span> developer </span> specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend and <span className="fw-bold" style={{color:"#3776ab"}}> Pyt</span><span className="fw-bold" style={{color: "#ffd343"}}>hon </span> backend development.
+        I am a <span className="fw-bold" style={{color: "#128C7E"}}>Full-stack</span> & <span className="fw-bold" style={{color: "#7B61FF"}}> LLM Applications </span> developer specializing in <span className="fw-bold" style={{color:"#61DBFB"}}> React </span> frontend and <span className="fw-bold" style={{color:"#3776ab"}}> Pyt</span><span className="fw-bold" style={{color: "#ffd343"}}>hon </span> backend development.
         <br></br>
         <br></br>
         Outside tech, I enjoy remixing tracks in DAWs — giving familiar tunes my own twist.
